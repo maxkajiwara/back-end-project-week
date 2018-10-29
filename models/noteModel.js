183bfb60de090d9b359a86a9faefec3d761ea778
Max <maxkajiwara@gmail.com>
2018-10-29 13:07:30 -0700

    Set up noteModel and noteRoutes

---
+++ models/noteModel.js
@@ -0,0 +1,16 @@
+const db = require('../config/dbConfig');
+
+module.exports = {
+	getNotes,
+	addNote,
+	updateNote,
+	deleteNote
+};
+
+function getNotes(user) {}
+
+function addNote(credentials) {}
+
+function updateNote(credentials) {}
+
+function deleteNote(credentials) {}
---
+++ routes/noteRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Models
+const noteModel = require('../models/noteModel');
+
+// get notes
+
+// get note by id
+
+// add note
+
+// update note
+
+// delete note
+
+module.exports = router;
