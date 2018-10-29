a92fb9def03645c59f3a10c5af7225733db7b2a8
Max <maxkajiwara@gmail.com>
2018-10-29 13:19:54 -0700

    Add GET /notes/

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -6,6 +6,14 @@ const router = express.Router();
 const noteModel = require('../models/noteModel');
 
 // get notes
+router.get('/', (req, res) => {
+	noteModel
+		.getNotes()
+		.then(notes => {
+			res.status(201).json(notes);
+		})
+		.catch(err => res.status(500).json(err));
+});
 
 // get note by id
 
