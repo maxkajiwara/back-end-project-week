168be0e8f8b477d206d5d407ad9f66b6c49d10f6
Max <maxkajiwara@gmail.com>
2018-10-29 14:58:30 -0700

    Update dbConfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,4 @@
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 
-const dbConfig = knex(knexConfig.development);
-
-module.exports = {
-	dbConfig
-};
+module.exports = knex(knexConfig.development);
